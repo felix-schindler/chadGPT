820392f2a06531c1cc4b4ddb4092093a1df54601
Felix <fs146@hdm-stuttgart.de>
2023-05-17 12:37:29 +0200

    Create empty app

---
+++ ChadGPT/ChadGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChadGPTApp.swift
+//  ChadGPT
+//
+//  Created by Felix Schindler on 17.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ChadGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
