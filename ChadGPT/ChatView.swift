d84f9f652c4a2a930dd473c45d68656807b61a9a
Felix <fs146@hdm-stuttgart.de>
2023-05-17 15:17:03 +0200

    ChatView: Fix padding (only apply horizontally)

--- ChadGPT/ChatView.swift
+++ ChadGPT/ChatView.swift
@@ -30,6 +30,12 @@ struct ChatView: View {
         Message(sender: .bot, content: "No problem"),
         Message(sender: .human, content: "Write a longer text"),
         Message(sender: .bot, content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
+        Message(sender: .human, content: "F"),
+        Message(sender: .bot, content: "F"),
+        Message(sender: .human, content: "F"),
+        Message(sender: .bot, content: "F"),
+        Message(sender: .human, content: "F"),
+        Message(sender: .bot, content: "F"),
     ]
     
     @State var msg = ""
@@ -63,7 +69,7 @@ struct ChatView: View {
                     Label("Settings", systemImage: "gearshape")
                 })
             }.sheet(isPresented: $showSettings, content: { ChatSettingsView(name: $name, show: $showSettings) })
-                .padding()
+                .padding(.horizontal)
                 .navigationTitle("Chat with \(name)")
         }
     }
